9b5a9a63f6c053709f863a4a727ffad781c1f171
okada_tech <okada.tecnorogy@mail.com>
2019-10-18 18:26:25 +0900

    ヒーロ射出モーターピン変更（Tim8-ch1,2）

--- F427IIH_Saramander_Hero/Inc/tim.h
+++ F427IIH_Saramander_Hero/Inc/tim.h
@@ -33,6 +33,7 @@
 extern TIM_HandleTypeDef htim1;
 extern TIM_HandleTypeDef htim2;
 extern TIM_HandleTypeDef htim6;
+extern TIM_HandleTypeDef htim8;
 extern TIM_HandleTypeDef htim12;
 
 /* USER CODE BEGIN Private defines */
@@ -42,10 +43,11 @@ extern TIM_HandleTypeDef htim12;
 void MX_TIM1_Init(void);
 void MX_TIM2_Init(void);
 void MX_TIM6_Init(void);
+void MX_TIM8_Init(void);
 void MX_TIM12_Init(void);
                         
 void HAL_TIM_MspPostInit(TIM_HandleTypeDef *htim);
-                                                            
+                                                                                
 /* USER CODE BEGIN Prototypes */
 
 /* USER CODE END Prototypes */
